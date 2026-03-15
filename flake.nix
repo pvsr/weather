e25eda074b31792f1f9d64a6b5de854adcb7cb8a
Peter Rice <peterrice@fastmail.com>
2026-03-15 12:03:14 -0400

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,93 +1,56 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    pre-commit-hooks,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages.default = with pkgs.python3Packages;
-          buildPythonPackage {
-            name = "weather";
-            format = "pyproject";
-            src = ./.;
-            propagatedBuildInputs = [
-              setuptools
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./module.nix
+      ];
 
-              flask
-              requests
-              cachecontrol
-            ];
-          };
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-        devShells.default = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-          buildInputs = with pkgs;
-            [
-              (python3.withPackages (ps:
-                with ps; [
+      perSystem =
+        {
+          config,
+          pkgs,
+          self',
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self'.packages.default ];
+            buildInputs = with pkgs; [
+              (python3.withPackages (
+                ps: with ps; [
                   mypy
                   black
                   pylsp-mypy
                   python-lsp-black
-                ]))
-            ]
-            ++ self.packages.${system}.default.propagatedBuildInputs;
-        };
+                ]
+              ))
+            ];
+          };
 
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              alejandra.enable = true;
-              black.enable = true;
-              ruff.enable = true;
+          packages.default =
+            with pkgs.python3Packages;
+            buildPythonPackage {
+              name = "weather";
+              format = "pyproject";
+              src = ./.;
+              propagatedBuildInputs = [
+                setuptools
+
+                flask
+                requests
+                cachecontrol
+              ];
             };
-          };
+
+          formatter = pkgs.nixfmt-tree;
         };
-      }
-    )
-    // {
-      nixosModules.default = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }: let
-        cfg = config.services.weather;
-        weather = self.outputs.packages."${pkgs.system}".default;
-      in {
-        options = {
-          services.weather = with lib; {
-            enable = mkEnableOption "weather";
-            bind = mkOption {
-              type = types.str;
-              default = "localhost:5997";
-            };
-          };
-        };
-        config = lib.mkIf (cfg.enable) {
-          systemd.services.weather = {
-            inherit (cfg) enable;
-            serviceConfig = {
-              DynamicUser = true;
-              ExecStart = ''
-                ${pkgs.python3Packages.gunicorn}/bin/gunicorn -b ${cfg.bind} weather.app
-              '';
-            };
-            environment = {
-              PYTHONPATH = weather.pythonModule.pkgs.makePythonPath [weather];
-            };
-            wantedBy = ["multi-user.target"];
-            after = ["network.target"];
-          };
-        };
-      };
     };
 }
---
+++ module.nix
@@ -0,0 +1,42 @@
+{ self, ... }:
+{
+  flake.nixosModules.default =
+
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      cfg = config.services.weather;
+      weather = self.outputs.packages."${pkgs.system}".default;
+    in
+    {
+      options = {
+        services.weather = with lib; {
+          enable = mkEnableOption "weather";
+          bind = mkOption {
+            type = types.str;
+            default = "localhost:5997";
+          };
+        };
+      };
+      config = lib.mkIf (cfg.enable) {
+        systemd.services.weather = {
+          inherit (cfg) enable;
+          serviceConfig = {
+            DynamicUser = true;
+            ExecStart = ''
+              ${pkgs.python3Packages.gunicorn}/bin/gunicorn -b ${cfg.bind} weather.app
+            '';
+          };
+          environment = {
+            PYTHONPATH = weather.pythonModule.pkgs.makePythonPath [ weather ];
+          };
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+        };
+      };
+    };
+}
